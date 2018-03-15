2735ca88ab1c32e3a1917a2dc71423e05f1b2934
jixcheng <jixcheng@gmail.com>
2018-03-14 21:45:41 -0400

    Javascript for main.js
    -constrains on display historical data

--- CongressionalDistricts/res/js/main.js
+++ CongressionalDistricts/res/js/main.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 
         var selectedId = $(this).prop('id');
         var selectedValue = $(this).val();
+        var DisplayHistoricalMap="DisplayHistoricalMap"
         switch (selectedId){
             case "priority-high":
                 array[0] = selectedValue;
+                // console.log($(this).val());
                 break;
             case "priority-med":
                 array[1] = selectedValue;
@@ -17,10 +19,35 @@ $(document).ready(function() {
                 array[2] = selectedValue;
                 break;
                           }
+              // console.log($(this).val());
+              if($(this).val() == DisplayHistoricalMap)
+              {
+                $('.selectprio1 option').each(function() {
+                    $(this).prop('disabled', true);
+                });
+                $('.selectprio2 option').each(function() {
+                    $(this).prop('disabled', true);
+                });
+              }
+              else{
+                $('.selectprio1 option').each(function() {
+                    $(this).prop('disabled', false);
+                });
+                $('.selectprio2 option').each(function() {
+                    $(this).prop('disabled', false);
+                });
+              }
 
-        $('.selectprio option').each(function() {
-            $(this).prop('disabled', false);
-        });
+          $('.selectprio option').each(function() {
+              $(this).prop('disabled', false);
+          });
+
+
+
+
+
+
+
 
         $('.selectprio option').each(function() {
             for (i = 0; i < array.length; i++) {
