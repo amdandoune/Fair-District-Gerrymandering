005a22782d42a47cb25cda84dd201fb9871ec2f7
Billy Kong <billyk142@gmail.com>
2018-03-14 22:28:30 -0400

    major

--- CongressionalDistricts/res/js/main.js
+++ CongressionalDistricts/res/js/main.js
@@ -6,55 +6,38 @@ $(document).ready(function() {
 
         var selectedId = $(this).prop('id');
         var selectedValue = $(this).val();
-        var DisplayHistoricalMap="DisplayHistoricalMap"
-        switch (selectedId){
-            case "priority-high":
-                array[0] = selectedValue;
-                // console.log($(this).val());
-                break;
-            case "priority-med":
-                array[1] = selectedValue;
-                break;
-            case "priority-low":
-                array[2] = selectedValue;
-                break;
-                          }
-              // console.log($(this).val());
-              if($(this).val() == DisplayHistoricalMap)
-              {
-                $('.selectprio1 option').each(function() {
-                    $(this).prop('disabled', true);
-                });
-                $('.selectprio2 option').each(function() {
-                    $(this).prop('disabled', true);
-                });
-              }
-              else{
-                $('.selectprio1 option').each(function() {
-                    $(this).prop('disabled', false);
-                });
-                $('.selectprio2 option').each(function() {
-                    $(this).prop('disabled', false);
-                });
-              }
-
-          $('.selectprio option').each(function() {
-              $(this).prop('disabled', false);
-          });
 
 
+        if (selectedValue == "Historical") {
+            $('#priority-med').prop("disabled", true);
+            $('#priority-low').prop("disabled", true);
+        }
+        else {
+            $('#priority-med').prop("disabled", false);
+            $('#priority-low').prop("disabled", false);
+            switch (selectedId){
+                case "priority-high":
+                    array[0] = selectedValue;
+                    break;
+                case "priority-med":
+                    array[1] = selectedValue;
+                    break;
+                case "priority-low":
+                    array[2] = selectedValue;
+                    break;
+                              }
 
+            $('.selectprio option').each(function() {
+                $(this).prop('disabled', false);
+            });
 
-
-
-
-
-        $('.selectprio option').each(function() {
-            for (i = 0; i < array.length; i++) {
-                if ($(this).val() == array[i]) {
-                    $(this).prop('disabled', true);
+            $('.selectprio option').each(function() {
+                for (i = 0; i < array.length; i++) {
+                    if ($(this).val() == array[i]) {
+                        $(this).prop('disabled', true);
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 });
