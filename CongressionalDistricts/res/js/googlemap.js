d7120c40bd97a65a62b8ca69add4ff54d2050df3
jixcheng <36716035+jixcheng@users.noreply.github.com>
2018-03-31 23:38:45 -0400

    added Missorui
    -added GeoJson
    -added JS

--- CongressionalDistricts/res/js/googlemap.js
+++ CongressionalDistricts/res/js/googlemap.js
@@ -95,7 +95,18 @@ $(document).ready(function() {
                 break;
 
             case 'Maine':
-                processLocation(43.65328, -70.276172, maine, 7, "#434444");
+                processLocation(43.65328, -70.276172, maine, 8, "#434444");
+                $('#pop-label').text('1,052,567');
+                $('#ethnicity-label').text('hispanic or latino(130,655), white(856,869), african american(60,189), asians(30,457)');
+                $('#party-label').text("Democrat");
+                $('#male-pop-label').text('508,400');
+                $('#female-pop-label').text("544,167");
+                $('#over18-label').text('828,611');
+                $('#under18-label').text('223,956');
+                break;
+
+            case 'Missouri':
+                processLocation(38.627003, -90.199402, missouri, 8, "#434444");
                 $('#pop-label').text('1,052,567');
                 $('#ethnicity-label').text('hispanic or latino(130,655), white(856,869), african american(60,189), asians(30,457)');
                 $('#party-label').text("Democrat");
